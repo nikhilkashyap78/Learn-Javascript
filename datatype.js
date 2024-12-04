74b335423fc7015fc996285dab6d565ac6ef1871
nikhilkashyap78 <nikhilrkashyap26@gmail.com>
2024-12-04 16:50:46 +0530

    datatype

---
+++ datatype.js
@@ -0,0 +1,23 @@
+// premitive datatype - 7
+// number, string, boolean, undefine, null, bigint, symbol
+
+let age = 23;
+console.log(typeof age)
+
+//non premetive
+// object- collection of values
+//key : value
+// age : 24 
+
+const student = {
+    fullName : "Nik K",
+    age : 23,
+    mark : 19,
+    isPass : true
+};
+
+// can update th evalue of key in const object
+console.log(student);
+console.log(student["mark"]);
+console.log(typeof student);
+
--- variables.js
+++ variables.js
@@ -15,3 +15,43 @@ console.log(isfollow)
 
 z = false;
 console.log(z);
+
+/////////////////////////////////////////////////////////////////
+
+// Naming Cases
+
+fullName = "Nikhil- variable"; //camel case, only this one
+//full_name //snake case             
+//full-name //kabab case
+//FullName  //pascal case
+//fullname //no name
+
+///////////////////////////////////////////////////////////////////
+// let, const & var // ES6-Ekma Script 
+
+// var: can be re-declared and updated, a global scope
+// let: canot re declared but can be updated, a block scope variable
+// const: cannot be re declared or updated, a block scope variable
+
+//const need initialize but let doen't need it
+
+// mostly use let and const //
+
+const fullName = "nik";
+let age = 23;
+var price 
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
