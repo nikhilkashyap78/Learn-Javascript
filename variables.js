f699e268d4dad5da4b04cfef76c00bdc658c3c50
nikhilkashyap78 <nikhilrkashyap26@gmail.com>
2024-12-04 15:29:12 +0530

    variable_boolean

---
+++ variables.js
@@ -0,0 +1,17 @@
+//variables and booliean
+
+name = "Nikhil- variable";
+console.log(name);
+
+num = 3047723502;
+console.log(num);
+
+x = null;
+y = undefined;
+console.log(x,y);
+
+isfollow = true;
+console.log(isfollow)
+
+z = false;
+console.log(z);
