5b44979cc046828a38e791edc6a4b107c3c80f11
nikhilkashyap78 <nikhilrkashyap26@gmail.com>
2024-12-06 01:19:23 +0530

    string

---
+++ String/string.js
@@ -0,0 +1,12 @@
+let str = "nik";
+
+let str2 = 'nik';
+
+console.log(str.length); //TO get th e length
+
+console.log(str[2]); //Index og string
+
+//Template Literals
+
+let secialString = 'this is a  '
+
