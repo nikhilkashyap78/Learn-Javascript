1670abfa8ea5a634e1464e670326776b57f1e404
nikhilkashyap78 <nikhilrkashyap26@gmail.com>
2024-12-06 02:16:03 +0530

    string method

--- String/string.js
+++ String/string.js
@@ -1,12 +1,32 @@
-let str = "nik";
+//string are emutable
 
+
+let str = "nik";
 let str2 = 'nik';
 
 console.log(str.length); //TO get th e length
-
 console.log(str[2]); //Index og string
 
 //Template Literals
 
-let secialString = 'this is a  '
+let secialString = 'this is a template literals'
 
+let obj = {
+    nam: "nik",
+    no: 23
+}
+
+//normal using comma
+console.log("the name of stuednt is",obj.nam,"and the number is",obj.no);
+
+//with template literals
+let output = `the name of student is ${obj.nam} and the number is ${obj.no}`;
+console.log(output);
+// ${expression} called place-holder this called string interpolation
+
+//scape character - count as single character
+
+//for next line
+console.log("nikhil\nkashyap")
+//for Tab
+console.log("nikhil\tkashyap") //
---
+++ String/string_method.js
@@ -0,0 +1,10 @@
+//built in function to manipulate string
+
+//str.toUpperCase()
+//str.lowerCase()
+//str.trim()
+
+let str = " nikhil kap hyap abc"
+console.log(str.toUpperCase())  //it doent change the orignal string, because string are emutable
+
+console.log(str.trim())
