d5b8b0448c3620d363233f158ede6f7041b0fe65
nikhilkashyap78 <nikhilrkashyap26@gmail.com>
2024-12-06 01:03:09 +0530

    loop

--- Loops/practise_2.js
+++ Loops/practise_2.js
@@ -1,1 +1,7 @@
-  
+let gameNum = 25;
+let userNum = prompt("Guess the no. : ");
+
+while(userNum != gameNum){
+    userNum = prompt("you entered wrong no, try again  : ");
+}
+console.log("congrasts you entered th eright no.");
