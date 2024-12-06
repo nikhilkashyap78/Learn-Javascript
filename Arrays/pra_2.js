5ced5f72676c95963618eba248ce6a5b80f2342e
nikhilkashyap78 <nikhilrkashyap26@gmail.com>
2024-12-07 01:59:19 +0530

    array

--- Arrays/pra_2.js
+++ Arrays/pra_2.js
@@ -1,12 +1,19 @@
-let arr = [250,645, 300, 900, 50];
-let a = 0
-for(let i=0; i<arr.length; i++){
-   a += arr[i]
-}
-console.log(a)
+ let arr = [250,645, 300, 900, 50];
+// let a = 0
+// for(let i=0; i<arr.length; i++){
+//    a += arr[i]
+// }
+// console.log(a)
 
-let x = 0
-for(let x of arr[x]){
-    x += arr[x]
+///////////////////////////////////////////////////////////
+
+ let x = 0;
+// let y =0;
+for(let value of arr){
+    x++
+    
+    console.log(arr[x])
+
+
 }
-console.log(x)
+//console.log(x)
