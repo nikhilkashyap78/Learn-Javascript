98cd0b131270cde3ced1802170e9537adb86ee57
nikhilkashyap78 <nikhilrkashyap26@gmail.com>
2024-12-07 01:40:00 +0530

    array

--- Arrays/pra_2.js
+++ Arrays/pra_2.js
@@ -1,3 +1,12 @@
 let arr = [250,645, 300, 900, 50];
+let a = 0
+for(let i=0; i<arr.length; i++){
+   a += arr[i]
+}
+console.log(a)
 
-for 
+let x = 0
+for(let x of arr[x]){
+    x += arr[x]
+}
+console.log(x)
