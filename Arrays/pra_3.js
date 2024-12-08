d91c598d4fe91bd22dad4c3a0b7345aa3fda883a
nikhilkashyap78 <nikhilrkashyap26@gmail.com>
2024-12-08 22:56:27 +0530

    pra_3

--- Arrays/pra_3.js
+++ Arrays/pra_3.js
@@ -1,8 +1,20 @@
 let itm = [23,4,2,5,23,21];
 
 let idx=0;
-for (let val of itm){
+for (let val of itm){   //val contain the value of itm at index
 
-    console.log(`value at index &{idx} = ${val}`);
+    console.log(`value at index ${idx} = ${val}`);
+
+    //give the 10% of each value 
+    let offer = val/10;
+
+    itm[idx] = itm[idx]-offer;
+
+    //we print the itm[idx] not the val because its the part of loop
+    console.log(`ater offer price = ${itm[idx]}`);
     idx++;
-}
+} 
+
+
+//////////////////////for loop/////////////////////////
+
