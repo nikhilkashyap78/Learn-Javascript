24900637ba19d4a0309464676bc266b7f73bcbe0
nikhilkashyap78 <nikhilrkashyap26@gmail.com>
2024-12-09 00:01:04 +0530

    array method

---
+++ Arrays/Array Methods/array_method.js
@@ -0,0 +1,14 @@
+//push() -Add
+//change in exiting array
+
+let foodItems = ["Apple", "Mango", "Orange"];
+
+foodItems.push("banana","abc")
+
+console.log(foodItems)
+
+//pop() -delete 
+
+deletedItem = foodItems.pop();
+console.log(foodItems)
+console.log(deletedItem) //print deleted item
