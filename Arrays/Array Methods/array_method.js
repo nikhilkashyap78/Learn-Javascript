a5adc7328ce8751c98276a903d5cc30b35bad89f
nikhilkashyap78 <nikhilrkashyap26@gmail.com>
2024-12-09 23:45:55 +0530

    array method

--- Arrays/Array Methods/array_method.js
+++ Arrays/Array Methods/array_method.js
@@ -7,12 +7,52 @@ foodItems.push("banana","abc")
 
 console.log(foodItems)
 
+///////////////////////////////////////////////////////////
 //pop() -delete 
+//change in exiting array
 
 deletedItem = foodItems.pop();
 console.log(foodItems)
 console.log(deletedItem) //print deleted item
 
-//toString
+///////////////////////////////////////////////////////////
+//toString()
 //create new string
 console.log(foodItems.toString()); //create the new string from the array
+
+////////////////////////////////////////////////////////////
+//concat()
+//create new array
+
+let arr1 = ["a","d","ww","we"];
+let arr2 = ["ds","sdf","df"];
+
+let arr3 = arr1.concat(arr2);
+console.log(arr3);
+
+////////////////////////////////////////////////////////////////
+//unshift() : add elelment in begnining
+a = arr1.unshift("aaaa");
+console.log(arr1)
+
+//shift() : delete at start
+b = arr1.shift();
+console.log(arr1)
+
+//////////////////////////////////////////////////////////////////
+
+//slice() : returna piece of array
+//slice(startIdx, endIdx)
+//No change in orignal array
+
+let car = ["bmw", "audi", "farari", "thar"];
+console.log(car)
+
+console.log(car.slice(0,2));
+
+//splice() : change orignal array (add, remove, replace)
+//splice(startIdx, delCount, newElement....)
+
+console.log(car.splice(0,2,"omni"))
+ 
+  
