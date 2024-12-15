de1b9f4c88758a618964920223a3973e621029c2
nikhilkashyap78 <nikhilrkashyap26@gmail.com>
2024-12-15 23:53:29 +0530

    forEach pra

---
+++ Loops/forEach.js
@@ -0,0 +1,27 @@
+// method are associate with any data structure 
+
+
+//array.forEach(callBackFunction)
+
+// CallBackFuntion : Here it is a fuuntion to execute for each
+// element in the array
+
+// A callback is a funtion passed as an argument to another function
+
+// arr.array.forEach(element) => {
+
+//     console.log(val);
+    
+// };
+
+//only for array
+
+let arr=[4,6,8,9];
+
+arr.forEach(function printVal(val){
+    console.log(val);
+
+});
+
+//higher order function : use other funtion as paramerter or return function
+
