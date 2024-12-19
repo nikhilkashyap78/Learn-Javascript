0cd7b8ec0b817872f6828272aa23ac8825ee3579
nikhilkashyap78 <nikhilrkashyap26@gmail.com>
2024-12-20 02:08:33 +0530

    map method

---
+++ Arrays/Array Methods/map_method.js
@@ -0,0 +1,10 @@
+//create new array with some result of operation 
+//value its callback return are useďto from new array
+
+//arr.map(callbackFnx(value, index, array))
+
+let num = [4,6,2,5,8,3,2];
+
+num.map((val) => {
+    console.log(val)
+})
