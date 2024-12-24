7be96513871cf3b168fb7d61102560d0c6a1cace
nikhilkashyap78 <nikhilrkashyap26@gmail.com>
2024-12-24 13:28:19 +0530

    attributes

---
+++ DOM_Concept/Attributes/attribute.js
@@ -0,0 +1,19 @@
+// Attributes 
+
+// getAttribute(attr) to get attribute value
+// setAttributes(attr, value) : to set attribute value
+
+let div = document.querySelector("div");
+console.dir(div);
+
+let id = div.getAttribute("id");
+console.log(id);
+ 
+console.log(div.setAttribute("id", "new_box"));
+
+
+
+
+
+
+
