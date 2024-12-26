a4561d43d5530eb19c886f2c5c3b2ef3d7e8ada9
nikhilkashyap78 <nikhilrkashyap26@gmail.com>
2024-12-26 13:36:08 +0530

    Event Object

--- DOM_Concept/script.js
+++ DOM_Concept/script.js
@@ -1,12 +1,27 @@
 let btn1 = document.querySelector("#btn1");
 
-btn1.onclick = () =>{
+btn1.onclick = (eve) =>{
+    console.log(eve);
     console.log("btn click")
     alert("hiiiiiiiii")
 };
 
-let box1 = document.querySelector("box1");
+let box1 = document.querySelector("#box1");
 
 box1.onmouseover = () =>{
-  console.log("hover")
+  console.log("hover");
+}
+
+/////////////////////////////////////////////////////
+
+//Event Object: detail about event
+
+// box1.event = (e) =>{
+//   //handel here
+// }
+
+btn1.onclick = (eve) =>{
+  console.log(eve);
+  console.log(eve.type);
+
 }
