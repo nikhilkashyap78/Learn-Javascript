d5486affe6ec9a0676e208628ed26e328b82c617
nikhilkashyap78 <nikhilrkashyap26@gmail.com>
2024-12-26 13:21:08 +0530

    event practical

--- DOM_Concept/script.js
+++ DOM_Concept/script.js
@@ -1,1 +1,12 @@
-  console.dir(window)   
+let btn1 = document.querySelector("#btn1");
+
+btn1.onclick = () =>{
+    console.log("btn click")
+    alert("hiiiiiiiii")
+};
+
+let box1 = document.querySelector("box1");
+
+box1.onmouseover = () =>{
+  console.log("hover")
+}
