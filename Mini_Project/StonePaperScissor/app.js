644dafb285e4f275d38d9dbbc505716729dc2653
nikhilkashyap78 <nikhilrkashyap26@gmail.com>
2025-01-07 16:51:29 +0530

    Game Result

--- Mini_Project/StonePaperScissor/app.js
+++ Mini_Project/StonePaperScissor/app.js
@@ -5,13 +5,26 @@ const choices = document.querySelectorAll(".choice");
 
 const generateCompChoice = () => {
     const options = ["rock", "paper", "scissor"];
-    Math.random() * 10;
-
+    const randIdx = Math.floor(Math.random() * 3);
+    return options[randIdx];
 };
+
+const drawGame = () => {
+    console.log("Game was Draw");
+}
 
 const playGame = (userChoice) => {
         console.log("user choice", userChoice);
         //Generate Computer Choice
+        const compChoice = generateCompChoice();
+        console.log("comp choice", compChoice);
+
+        if(userChoice===compChoice){
+            //draw game
+            drawGame();
+        }else{
+            //user
+        }
 
 }
 
@@ -20,7 +33,6 @@ choices.forEach((choice) => {
     choice.addEventListener("click",()=>{
 
         const userChoice = choice.getAttribute("id");
-        console.log("clicked", userChoice);
         playGame(userChoice);
     });
 });
