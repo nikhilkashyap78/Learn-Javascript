3c3d39e9d5c29394aad0710374655ba68519e1cc
nikhilkashyap78 <nikhilrkashyap26@gmail.com>
2025-01-07 13:44:20 +0530

    Computer Choice

--- Mini_Project/StonePaperScissor/app.js
+++ Mini_Project/StonePaperScissor/app.js
@@ -1,12 +1,27 @@
 let userScore = 0;
 let comScore = 0;
 
-const choices = document.querySelectorAll("choices");
+const choices = document.querySelectorAll(".choice");
+
+const generateCompChoice = () => {
+    const options = ["rock", "paper", "scissor"];
+    Math.random() * 10;
+
+};
+
+const playGame = (userChoice) => {
+        console.log("user choice", userChoice);
+        //Generate Computer Choice
+
+}
 
 choices.forEach((choice) => {
-    console.log(choice);
-    choice.addEventListener("click",()=>{
-        console.log("clicked");
-    });
 
+    choice.addEventListener("click",()=>{
+
+        const userChoice = choice.getAttribute("id");
+        console.log("clicked", userChoice);
+        playGame(userChoice);
+    });
 });
+
