085d212a246d9600e131d9ba531b8e56f84b124d
nikhilkashyap78 <nikhilrkashyap26@gmail.com>
2025-01-08 17:38:05 +0530

    Prototype

---
+++ Classes_&_Objects/prototype.js
@@ -0,0 +1,14 @@
+const employee = {
+    calTax1() {
+        console.log("Tax is 10%");
+    },
+
+    // calTax2 : function() {
+    //     console.log("Tax is 10%"); //other way of writing the above function
+    // }
+};
+
+const nik = {
+    salary: 100000,
+
+};
