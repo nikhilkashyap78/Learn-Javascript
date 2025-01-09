8ead28ad8cae0425b3b96f41ae94b49a13541fab
nikhilkashyap78 <nikhilrkashyap26@gmail.com>
2025-01-09 13:26:15 +0530

    classes

---
+++ Classes_&_Objects/classes.js
@@ -0,0 +1,8 @@
+// Class is a program-code template for creating objects.
+// Those obj will have some state (variables) and behavior (function) inside it.
+
+// Class is a blueprint for creating objects.
+
+class MyClass{
+    
+}
--- Classes_&_Objects/prototype.js
+++ Classes_&_Objects/prototype.js
@@ -1,3 +1,9 @@
+// A JS obj is entity having state and behavior (properties and methods).
+// JS obj have special property called prototype.
+// We can set prototype of an obj using __proto__ property.
+
+// If obj & prototype have same method, obj method is called first.
+
 const employee = {
     calTax1() {
         console.log("Tax is 10%");
@@ -12,3 +18,5 @@ const nik = {
     salary: 100000,
 
 };
+
+nik.__proto__ = employee;
