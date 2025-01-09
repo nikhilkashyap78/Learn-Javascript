2c719fc2f862091a8fc0ab8bc318d9628e059590
nikhilkashyap78 <nikhilrkashyap26@gmail.com>
2025-01-09 13:52:32 +0530

    Constructor

--- Classes_&_Objects/classes.js
+++ Classes_&_Objects/classes.js
@@ -3,6 +3,38 @@
 
 // Class is a blueprint for creating objects.
 
-class MyClass{
-    
+// class MyClass{
+//     constructor(){...};
+//     myMethod(){...};
+// }
+
+// let myObj = new MyClass();
+
+class myCar{
+
+    start(){
+        console.log("Car started");
+    }
+    stop(){
+        console.log("Car stopped");
+    }
+
+    setBrand(brand){
+        this.brand = brand;
+    }
 }
+
+let fortuner = new myCar();
+fortuner.setBrand("Frotuner");
+
+let bmw = new myCar();
+bmw.setBrand("BMW");
+
+
+//Constructor() 
+// - It is a special method in class.
+// - It is called when obj is created.
+// - It is used to initialize obj.
+// - It is called only once.
+// - It is optional.
+// Invoked using new keyword.
