d55d89b6275a67482ebe62fe17812297c7d0ca5e
nikhilkashyap78 <nikhilrkashyap26@gmail.com>
2025-01-11 19:47:08 +0530

    callback function

---
+++ Callbacks, Promises & Async Await/callBack.js
@@ -0,0 +1,8 @@
+//Callback function: A callback function is a function that is passed as an argument to another function,
+// to be “called back” at a later time.
+// A callback function can run after another function has finished.
+
+sum(()=>{
+    console.log.apply(a+b)
+})
+
---
+++ Callbacks, Promises & Async Await/data.js
@@ -0,0 +1,40 @@
+// Sync in js : 
+// Sync code is executed in sequence - one after another
+
+console.log('1');
+console.log('2');
+console.log('3');
+console.log('4');
+console.log('5');
+console.log('6');
+
+// Async in js : 
+// Async code is executed in parallel - together
+  
+function hello() {
+    console.log('Hello');
+}
+
+setTimeout(hello, 3000); //timeout is 3 seconds
+
+////////////////////////////////////////////////////////////
+
+setTimeout(() => {            //other way to write function(arrow fun)
+    console.log('World');     //setTimeout(() => {}, 5000)
+}, 5000)
+
+
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
