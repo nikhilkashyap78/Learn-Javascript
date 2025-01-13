36059985cb3c16be19f42fd99a2e3591cd49fdaa
nikhilkashyap78 <nikhilrkashyap26@gmail.com>
2025-01-13 12:27:43 +0530

    Fetch API

---
+++ fetchapi/fetch.js
@@ -0,0 +1,5 @@
+// The fetch api provide an interface for fetching (send/recive) resources
+// It uses Request and Response objects
+// The fetch() method is used to fetch a resource(data)
+
+const fetch = require('node-fetch');
