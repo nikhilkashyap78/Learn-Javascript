62bf3533360db03f7b90f0e689874f518abbf86f
nikhilkashyap78 <nikhilrkashyap26@gmail.com>
2025-01-19 15:21:30 +0530

    Logic

---
+++ Mini_Project/CurrencyConverter/app.js
@@ -0,0 +1,7 @@
+  const BASE_URL = "https//:cdn.jsdeliver.net/npm/@fawazahmed0/currency-api@latest/v1/currencies/usd/inr.json";
+  
+  //https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies/usd.json
+
+const dropdown = document.getElementById(".dropdown select");
+
+let (code in countryList)
